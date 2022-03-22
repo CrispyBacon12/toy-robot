763cdb83f02cce73ca7671981c0c4fed721061db
Chris Purssell <chris.purssell@workday.com>
2022-03-23 09:34:47 +1000

    Remove unnecessary function binding
    This is no longer needed since adding useScene to

--- src/models/Scene.ts
+++ src/models/Scene.ts
@@ -4,9 +4,7 @@ import { Toy } from "./Toy";
 import { Vector } from "./Vector";
 
 export class Scene {
-  constructor(public readonly grid: Grid, public readonly toy?: Toy) {
-    this.place = this.place.bind(this);
-  }
+  constructor(public readonly grid: Grid, public readonly toy?: Toy) {}
 
   place(x: number, y: number, direction: Facing) {
     return new Toy(new Vector(x, y), Grid.directions[direction]);
